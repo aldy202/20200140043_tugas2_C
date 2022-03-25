44f616cdaad54662fd90556fda16d95c7726b6f8
aldy202 <aldyfauzi202@.gmail.com>
2022-03-25 23:51:16 +0700

    insert

--- 20200140043_tugas2_B/Program.cs
+++ 20200140043_tugas2_B/Program.cs
@@ -22,7 +22,12 @@ namespace _20200140043_tugas2_B
                 cn = new SqlConnection("data source=MEGANE\\ALDYAHMADFAUZI;database=aldyahmadfauzi;Integrated Security = TRUE");
                 cn.Open();
 
-                SqlCommand cm = new SqlCommand("Insert Into pegawai(Id_pegawai,Nama_pegawai,noHp,jenis_kelamin,alamat)VALUES ('00001','Aldy','087720302716','L','Pekalongan')", cn);
+                SqlCommand cm = new SqlCommand("Insert Into pegawai(Id_pegawai,Nama_pegawai,noHp,jenis_kelamin,alamat)VALUES ('00001','Aldy','087720302716','L','Pekalongan')" +
+                    "Insert Into pegawai(Id_pegawai, Nama_pegawai, noHp, jenis_kelamin, alamat)VALUES('00002','ahmad', '087720302718', 'L', 'sleman')"+
+                    "Insert Into pegawai(Id_pegawai,Nama_pegawai,noHp,jenis_kelamin,alamat)VALUES ('00003','Fauzi','087720302723','L','Yogyakarta')"+
+                    "Insert Into pegawai(Id_pegawai,Nama_pegawai,noHp,jenis_kelamin,alamat)VALUES ('00004','Wanti','087720302342','P','Bantul')"+ 
+                    "Insert Into pegawai(Id_pegawai,Nama_pegawai,noHp,jenis_kelamin,alamat)VALUES ('00005','sakrina','087720302345','P','Kasihan')"+
+                    "Insert Into pegawai(Id_pegawai,Nama_pegawai,noHp,jenis_kelamin,alamat)VALUES ('00006','murti','087720302743','P','Yogyakarta')", cn);
                 cm.ExecuteNonQuery();
 
                 Console.WriteLine("Sukses menambahkan data");
